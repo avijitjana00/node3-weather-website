af4712e1798e1659d1b461d36c54d8ee2c00af86
Avijit <avijitjana002016@gmail.com>
2022-12-18 19:49:51 +0530

    commit successful

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 console.log('client side javscript file is loaded')
 
+
 // fetch('https://puzzle.mead.io/puzzle').then((response) =>{
 //     response.json().then((data) =>{
 //         console.log(data)
