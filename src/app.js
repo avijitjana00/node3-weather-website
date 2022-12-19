f5d9954aa6fd5630c6fc1f657c46874aad4f5a04
Avijit <avijitjana002016@gmail.com>
2022-12-19 15:01:22 +0530

    done

--- src/app.js
+++ src/app.js
@@ -48,22 +48,25 @@ app.get('/weather',(req,res) =>{
             error:'you must provide an address!'
         })
     }
+    else{
+        address = (req.query.search)
 
-    geocode(req.query.address,(error,{latitude,longitude,location} ={}) =>{
-        if(error){
-            return res.send({error})
-        }
-        forecast(latitude,longitude,(error,forecastData) =>{
+        geocode(req.query.address,(error,{latitude,longitude,location} ={}) =>{
             if(error){
                 return res.send({error})
             }
-            res.send({
-                forecast:forecastData,
-                location,
-                address:req.query.address
+            forecast(latitude,longitude,(error,forecastData) =>{
+                if(error){
+                    return res.send({error})
+                }
+                res.send({
+                    forecast:forecastData,
+                    location,
+                    address:req.query.address
+                })
             })
         })
-    })
+    }
     // res.send({
     //     forecast:'It is snowing',
     //     location:'philadelphia',
