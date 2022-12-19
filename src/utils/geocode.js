e87a1cc6e1bf17955d2392f9448d6cd13a64ae68
Avijit <avijitjana002016@gmail.com>
2022-12-19 13:45:44 +0530

    done

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const geocode = (address,callback) =>{
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'Los%20Angeles.json?access_token=pk.eyJ1IjoiYXZpaml0MiIsImEiOiJjbGJpM2hua20wNWFlM25vOGF5dWNtZ3M2In0.gbLflsQ3twbGbGuMKQ4M4Q&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoiYXZpaml0MiIsImEiOiJjbGJpM2hua20wNWFlM25vOGF5dWNtZ3M2In0.gbLflsQ3twbGbGuMKQ4M4Q&limit=1'
     
     request({url,json:true},(error,{body}) =>{
         if(error){
